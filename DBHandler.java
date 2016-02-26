d75e7dc3d692e2cd073d84d4b1c665a9742e469f
Dean Harrison <DeanH544@gmail.com>
2016-02-26 10:07:34 +0000

    Update DBHandler.java
    Updated with how-to-use comments.

--- DBHandler.java
+++ DBHandler.java
@@ -1,3 +1,45 @@
+/**
+	To use this demo class please follow instructions from 
+
+HelloWorldTWo.java prior to these instructions.
+
+	Place the "DBHandler.java" file into your java folder, 
+
+then compile using the following command: "javac -d classes 
+
+DBHandler.java". 
+
+	Go back to your HelloWorldTwo.java file and un-comment 
+
+the import statement or write the following import statement: 
+
+"import com.deanharrison.database.DBHandler;" then create an 
+
+instance of the DBHandler class by using the following line of 
+
+code: "DBHandler db = new DBHandler();"
+
+	To compile your updated HelloWorldTwo.java file you 
+
+need to use the following compiler command: "java -d classes 
+
+-cp classes HelloWorldTwo.java", this will place the compiled 
+
+files into the classes folder but also look inside the classes 
+
+folder for any classes you may be referencing, in this case 
+
+the DBHandler.class will be found.
+
+	To run the HelloWorldTwo.java file is the same as 
+
+previously: "java -cp classes 
+
+com.deanharrison.test.HelloWorldTwo.java"
+
+	Any questions please don't hesitate to ask.
+*/
+
 package com.deanharrison.database;
 
 public class DBHandler{
